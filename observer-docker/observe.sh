01aad4d884d75319691f69ccc21085615e4463ed
Jan Gru <j4n6ru@gmail.com>
2020-06-16 11:24:01 +0200

    Extend observer functionality

--- observer-docker/observe.sh
+++ observer-docker/observe.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-# you might want to change the directory below into the targeted directory
-
 
 TSA="https://freetsa.org/tsr"
 #TSA="https://zeitstempel.dfn.de"
 tsr="tsr"
 
 observe(){
-    local watch_dir=$1
-    local work_dir=$2
-    local hashsum=$3
     local suf=${hashsum%sum}  # remove sum
 
     # Run inotify
@@ -21,38 +16,78 @@ observe(){
     do
 	if [[ -f $f ]];
 	then
-	    fn=$(basename "$f")
-	    # Echo basename
-	    echo "${fn} close_write"
+	    fn=${f#$(dirname ${f})/}
+	    # Echo filename
+	    #echo "${fn} close_write"
 
 	    # Ignore dot files
 	    if ! [[ "$fn" =~ ^\..* ]];
 	    then
 		# Ignore files with suffix .${hash}
-		if ! [[ $fn =~ .*\.($suf)$ ]];
+		if ! [[ $fn =~ .*\.($suf|$tsr)$ ]];
 		then
 		    # Calculate hash sum
-		    eval ${hash} $f > "${f}.${suf}";
-
+		    eval ${hashsum} $f > "${f}.${suf}";
+		    create_working_copy "${f}.${suf}"
+		    create_working_copy "${f}"
 		elif ! [[ $fn =~ .*\.($tsr)$ ]];
 		     then
 			 # Create openssl timestamp request by taking the hash of the hash file
 			 # See: https://books.google.cz/books?id=pgYvDwAAQBAJ&printsec=frontcover&hl=cs&source=gbs_ge_summary_r&cad=0#v=onepage&q=openssl&f=false
 			 # Pipe it to curl to submit b/m request to timestamping authoriy
-			 openssl ts -query -data $f.$suf -cert| \
+			 openssl ts -query -data $f -cert| \
 			     curl -s -H "Content-Type: application/timestamp-query" --data-binary @- \
-				  "$TSA" > $f.$suf.$SUF2
+				  "$TSA" > $f.$tsr
 
+			 # Initiate copy to work_dir after checksum and timestamp are created
+			 create_working_copy "${f}.${tsr}"
 		fi
 	    fi
 	fi
     done
 }
 
+create_working_copy(){
+    local dhash=""
+    local src=$1
+    local shash=$(eval ${hashsum} ${src} | awk '{print $1}')
+
+    # Make sure, that hashes of src and dst match, otherwise retry
+    while [[ ${dhash} != ${shash} ]]
+    do
+	dhash=$(hcopy ${src})
+    done;
+}
+
+hcopy() {
+    local src=$1
+    local rel_path=${src#${watch_dir}}
+    local dst=${work_dir}${rel_path}
+
+    #echo "Creating dir: $(dirname ${dst}) and starting copy"
+    mkdir -p $(dirname ${dst})
+    cp ${src} ${dst}
+
+    # Return hash of copied file
+    local result_hash=$(eval ${hashsum} ${dst} | awk '{print $1}')
+    echo ${result_hash}
+}
+
 ############################################################################
 # Main program
 # Uses positional arguments:
-# $1 = observed_dir, $2 = dir to copy to, $3 hashing method
+# $1 = dir to observe
+# $2 = dir to copy to
+# $3 hashing method to use (md5sum, sha256sum,...)
 ############################################################################
 
-observe $1 $2 $3
+hashsum=$3
+if ! [[ $hashsum ]]
+then
+    hashsum="md5sum"
+fi
+
+watch_dir=$1
+work_dir=$2
+
+observe
